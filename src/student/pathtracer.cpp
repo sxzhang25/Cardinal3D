293f1c43bbae74e497ebf42cdcebd019c224fb3f
Sharon Zhang <szhang25@stanford.edu>
2024-02-27 14:31:46 -0800

    fix task 1

--- src/student/pathtracer.cpp
+++ src/student/pathtracer.cpp
@@ -22,7 +22,7 @@ Spectrum Pathtracer::trace_pixel(size_t x, size_t y) {
 
     // If n_samples is 1, please send the ray through the center of the pixel.
     Ray out;
-    Samplers::Rect::Uniform uniform_sampler;
+    Samplers::Rect::Uniform uniform_sampler(Vec2(1.0f, 1.0f));
     if (n_samples == 1) {
         xy += Vec2(0.5f, 0.5f);
     } else {  // If n_samples > 1, please send the ray through any random point within the pixel
@@ -38,7 +38,7 @@ Spectrum Pathtracer::trace_pixel(size_t x, size_t y) {
     // Tip: you may want to use log_ray for debugging. Given ray t, the following lines
     // of code will log .03% of all rays (see util/rand.h) for visualization in the app.
     // see student/debug.h for more detail.
-    if (RNG::coin_flip(0.000003f))
+    if (RNG::coin_flip(0.0005f))
         log_ray(out, 10.0f);
 
     return trace_ray(out);
